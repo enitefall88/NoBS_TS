fc01af23fda2763dfe14e87528323046c2ef5462
Anton Durandin <enitefall81@gmail.com>
2022-04-08 12:50:57 -0700

    Initital commit

--- Challenge1/index.ts
+++ Challenge1/index.ts
@@ -9,22 +9,23 @@ interface HouseWithID  extends House{
   id: number
 }
 
+type FilterFunc = (house: House) => boolean
 
 function findHouses( // 2 parameters, one is input and the other is a function
   input: string | House[],
-  filter?: (house: House) => boolean
-): HouseWithID[] {
+  filter?: (house: House) => boolean //filter is optional
+): HouseWithID[] { // ): HouseWithID[] is output
   let houses: House[] = typeof input === 'string' ? JSON.parse(input) : input
+  console.log(houses)
   return (filter? houses.filter(filter) : houses) // if there is optional filter function
       .map((house) => ({  // why curly braces don`t work here?
             id: houses.indexOf(house),
             ...house
       }))
 }
+console.log( findHouses(JSON.stringify(houses)))
 
+console.log( findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides"))
 
-console.log(
-  findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
-);
-
-console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
+//console.log(findHouses(houses, ({ name }) => name === "Corrino"));
+//console.log(findHouses(houses))
