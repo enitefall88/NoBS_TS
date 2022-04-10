276ee035b3c459013b49378e624a52b034fa786e
Anton Durandin <enitefall81@gmail.com>
2022-04-10 15:33:22 -0700

    generics ranks

--- generics.ts
+++ generics.ts
@@ -1,11 +1,28 @@
-function simpleStringState(  //tracks a piece of stored space
-    initial: string  // initial is saved here
-): [()=> string, (v:string) => void] { // tuple of two functions in array because it`s two
-  let str: string = initial
+function simpleState<T>(  //tracks a piece of stored space
+    str: T  // initial is saved here
+): [()=> T, (v:T) => void] { // tuple of two functions in array because it`s two
+  let val: T = str
   return [
     () => str,
-    (v: string) => {
+    (v: T) => {
     str = v
   }
  ]
+}
+
+  interface Rank<RankItem> {
+    item: RankItem,
+    rank: number
+  }
+
+function ranker<RankItem>(items: RankItem[], rank: (v: RankItem) => number
+) : RankItem[]  { // <RankItem> is a generic type
+
+  let ranks: Rank<RankItem>[] = items.map((item) => ({ /// why curly braces do not work here?
+ item,
+ rank: rank(item)
+ }))
+
+  ranks.sort((a,b) => a.rank - b.rank)
+  return ranks.map(rank => rank.item)
 }
