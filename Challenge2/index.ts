d9f8916ae381ae87fa1b915cdb10b3ba97c4e59a
Anton Durandin <enitefall81@gmail.com>
2022-04-14 15:14:31 -0700

    Add reduce

--- Challenge2/index.ts
+++ Challenge2/index.ts
@@ -6,3 +6,15 @@ function myForEach<T>(items: T[], forEachFunc: (v: T)=> void): void {
 }
 
 myForEach(["a","b","c"], (v)=> console.log(`forEach ${v}`))
+
+
+
+
+
+let reducer = (array: number[]) => {
+array.reduce((acc, val) => {
+   return acc + val
+ })
+}
+
+console.log(reducer([1,2,3,4,5]))
