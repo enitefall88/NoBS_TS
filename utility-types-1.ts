a35148b2fbd3ca98c2ab2e845771d7e100bbdca8
Anton Durandin <enitefall81@gmail.com>
2022-04-26 15:52:01 -0700

    Omit

--- utility-types-1.ts
+++ utility-types-1.ts
@@ -1,19 +1,21 @@
 interface MyUser {
   name: string
-  id: string
+  id: number | string
   email?: string
 }
 
-interface MyUserOptionals {
+/*interface MyUserOptionals {
   name?: string
   id?: string
   email?: string
-}
+}*/
+
+type MyUserOptionals = Partial<MyUser>
 
 let merge = (user: MyUser, overrides: MyUserOptionals) => {
   return {
   ...user,
-  overrides
+  ...overrides
   }
 }
 
@@ -21,6 +23,40 @@ console.log(merge(
     {
       name:"Jack", id:"33", email:"epn@dsdsd"
     }, {
-    email: "new@email.com"
+    email: "new@email.com"// this is going to override the original email with a new one
     }
     )) // need a second argument for overrides
+
+type RequiredMyUser = Required<MyUser> // all is required
+
+type EmailAndName = Pick<MyUser, "name" | "email">
+
+let harry:EmailAndName = {name: "Harrison", email: "author@blah"} //
+
+console.log(harry)
+
+type UserWithoutId =  Omit<MyUser, "id">
+
+const mapById = (users: MyUser[]): Record<MyUser["id"], UserWithoutId> => {
+  return users.reduce((a, v) => {
+    const { id, ...other } = v; // other??
+    return {
+      ...a,
+      [id]: other,
+    };
+  }, {});
+};
+
+console.log(
+  mapById([
+    {
+      id: 1,
+      name: "Mr. Foo",
+    },
+    {
+      id: 2,
+      name: "Mrs. Baz",
+    },
+  ])
+);
+
